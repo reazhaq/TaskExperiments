9ee8bcd3061c2869bcbf6f326db87b5bf69cd72d
Haq, Reaz (CAI - Austin) <Reaz.Haq@coxautoinc.com>
2019-01-03 08:37:31 -0600

    foreach

---
+++ AddArrayElementsUsingThreadLocal/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AddArrayElementsUsingThreadLocal
+{
+    class Program
+    {
+        static void Main()
+        {
+            const int arrayLength = 10;//1000000
+            int[] nums = Enumerable.Range(0, arrayLength).ToArray();
+            long total = 0;
+
+            // Use type parameter to make subtotal a long, not an int
+            Parallel.For<long>(0, nums.Length, () => 0,
+                (j, loop, subtotal) =>
+                {
+                    Debug.WriteLine($"    j:{j}, loop:{loop}, subtotal:{subtotal}, tid:{Thread.CurrentThread.ManagedThreadId}");
+                    subtotal += nums[j];
+                    return subtotal;
+                },
+                (x) =>
+                {
+                    Debug.WriteLine($"  total:{total}, x:{x}, tid:{Thread.CurrentThread.ManagedThreadId}");
+                    Interlocked.Add(ref total, x);
+                    Debug.WriteLine($"   total:{total}, x:{x}, tid:{Thread.CurrentThread.ManagedThreadId}");
+                }
+            );
+
+            Console.WriteLine("The total is {0:N0}", total);
+
+            var partition = Partitioner.Create(0, arrayLength, 3);
+            Parallel.ForEach(partition, (range, loopState) =>
+            {
+                Debug.WriteLine($" range:{range}, loopState:{loopState}");
+                for(var item=range.Item1; item<range.Item2; item++)
+                {
+                    Debug.WriteLine($"     item:{item}");
+                }
+            });
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
--- MatrixMultiplication/Program.cs
+++ MatrixMultiplication/Program.cs
@@ -2,136 +2,139 @@
 using System.Diagnostics;
 using System.Threading.Tasks;
 
-class MultiplyMatrices
+namespace MatrixMultiplication
 {
-    #region Sequential_Loop
-    static void MultiplyMatricesSequential(double[,] matA, double[,] matB,
-                                            double[,] result)
+    class MultiplyMatrices
     {
-        int matACols = matA.GetLength(1);
-        int matBCols = matB.GetLength(1);
-        int matARows = matA.GetLength(0);
-
-        for (int i = 0; i < matARows; i++)
+        #region Sequential_Loop
+        static void MultiplyMatricesSequential(double[,] matA, double[,] matB,
+            double[,] result)
         {
-            for (int j = 0; j < matBCols; j++)
+            int matACols = matA.GetLength(1);
+            int matBCols = matB.GetLength(1);
+            int matARows = matA.GetLength(0);
+
+            for (int i = 0; i < matARows; i++)
             {
-                double temp = 0;
-                for (int k = 0; k < matACols; k++)
+                for (int j = 0; j < matBCols; j++)
                 {
-                   temp += matA[i, k] * matB[k, j];
+                    double temp = 0;
+                    for (int k = 0; k < matACols; k++)
+                    {
+                        temp += matA[i, k] * matB[k, j];
+                    }
+                    result[i, j] += temp;
                 }
-                result[i, j] += temp;
             }
         }
-    }
-    #endregion
+        #endregion
 
-    #region Parallel_Loop
-    static void MultiplyMatricesParallel(double[,] matA, double[,] matB, double[,] result)
-    {
-        int matACols = matA.GetLength(1);
-        int matBCols = matB.GetLength(1);
-        int matARows = matA.GetLength(0);
-
-        // A basic matrix multiplication.
-        // Parallelize the outer loop to partition the source array by rows.
-        Parallel.For(0, matARows, i =>
+        #region Parallel_Loop
+        static void MultiplyMatricesParallel(double[,] matA, double[,] matB, double[,] result)
         {
-            for (int j = 0; j < matBCols; j++)
+            int matACols = matA.GetLength(1);
+            int matBCols = matB.GetLength(1);
+            int matARows = matA.GetLength(0);
+
+            // A basic matrix multiplication.
+            // Parallelize the outer loop to partition the source array by rows.
+            Parallel.For(0, matARows, i =>
             {
-                double temp = 0;
-                for (int k = 0; k < matACols; k++)
+                for (int j = 0; j < matBCols; j++)
                 {
-                    temp += matA[i, k] * matB[k, j];
+                    double temp = 0;
+                    for (int k = 0; k < matACols; k++)
+                    {
+                        temp += matA[i, k] * matB[k, j];
+                    }
+                    result[i, j] = temp;
                 }
-                result[i, j] = temp;
-            }
-        }); // Parallel.For
-    }
-    #endregion
-
-
-    #region Main
-    static void Main(string[] args)
-    {
-        // Set up matrices. Use small values to better view 
-        // result matrix. Increase the counts to see greater 
-        // speedup in the parallel loop vs. the sequential loop.
-        int colCount = 180;
-        int rowCount = 2000;
-        int colCount2 = 270;
-        double[,] m1 = InitializeMatrix(rowCount, colCount);
-        double[,] m2 = InitializeMatrix(colCount, colCount2);
-        double[,] result = new double[rowCount, colCount2];
-
-        // First do the sequential version.
-        Console.Error.WriteLine("Executing sequential loop...");
-        Stopwatch stopwatch = new Stopwatch();
-        stopwatch.Start();
-
-        MultiplyMatricesSequential(m1, m2, result);
-        stopwatch.Stop();
-        Console.Error.WriteLine("Sequential loop time in milliseconds: {0}",
-                                stopwatch.ElapsedMilliseconds);
-
-        // For the skeptics.
-        OfferToPrint(rowCount, colCount2, result);
-
-        // Reset timer and results matrix. 
-        stopwatch.Reset();
-        result = new double[rowCount, colCount2];
-
-        // Do the parallel loop.
-        Console.Error.WriteLine("Executing parallel loop...");
-        stopwatch.Start();
-        MultiplyMatricesParallel(m1, m2, result);
-        stopwatch.Stop();
-        Console.Error.WriteLine("Parallel loop time in milliseconds: {0}",
-                                stopwatch.ElapsedMilliseconds);
-        OfferToPrint(rowCount, colCount2, result);
-
-        // Keep the console window open in debug mode.
-        Console.Error.WriteLine("Press any key to exit.");
-        Console.ReadKey();
-    }
-    #endregion
-
-    #region Helper_Methods
-    static double[,] InitializeMatrix(int rows, int cols)
-    {
-        double[,] matrix = new double[rows, cols];
-
-        Random r = new Random();
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < cols; j++)
-            {
-                matrix[i, j] = r.Next(100);
-            }
+            }); // Parallel.For
         }
-        return matrix;
-    }
+        #endregion
 
-    private static void OfferToPrint(int rowCount, int colCount, double[,] matrix)
-    {
-        //Console.Error.Write("Computation complete. Print results (y/n)? ");
-        //char c = Console.ReadKey(true).KeyChar;
-        //Console.Error.WriteLine(c);
-        //if (Char.ToUpperInvariant(c) == 'Y')
-        //{
-        //    if (! Console.IsOutputRedirected) Console.WindowWidth = 180;
-        //    Console.WriteLine();
-        //    for (int x = 0; x < rowCount; x++)
-        //    {
-        //        Console.WriteLine("ROW {0}: ", x);
-        //        for (int y = 0; y < colCount; y++)
-        //        {
-        //            Console.Write("{0:#.##} ", matrix[x, y]);
-        //        }
-        //        Console.WriteLine();
-        //    }
-        //}
+
+        #region Main
+        static void Main(string[] args)
+        {
+            // Set up matrices. Use small values to better view 
+            // result matrix. Increase the counts to see greater 
+            // speedup in the parallel loop vs. the sequential loop.
+            int colCount = 180;
+            int rowCount = 2000;
+            int colCount2 = 270;
+            double[,] m1 = InitializeMatrix(rowCount, colCount);
+            double[,] m2 = InitializeMatrix(colCount, colCount2);
+            double[,] result = new double[rowCount, colCount2];
+
+            // First do the sequential version.
+            Console.Error.WriteLine("Executing sequential loop...");
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            MultiplyMatricesSequential(m1, m2, result);
+            stopwatch.Stop();
+            Console.Error.WriteLine("Sequential loop time in milliseconds: {0}",
+                stopwatch.ElapsedMilliseconds);
+
+            // For the skeptics.
+            OfferToPrint(rowCount, colCount2, result);
+
+            // Reset timer and results matrix. 
+            stopwatch.Reset();
+            result = new double[rowCount, colCount2];
+
+            // Do the parallel loop.
+            Console.Error.WriteLine("Executing parallel loop...");
+            stopwatch.Start();
+            MultiplyMatricesParallel(m1, m2, result);
+            stopwatch.Stop();
+            Console.Error.WriteLine("Parallel loop time in milliseconds: {0}",
+                stopwatch.ElapsedMilliseconds);
+            OfferToPrint(rowCount, colCount2, result);
+
+            // Keep the console window open in debug mode.
+            Console.Error.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+        #endregion
+
+        #region Helper_Methods
+        static double[,] InitializeMatrix(int rows, int cols)
+        {
+            double[,] matrix = new double[rows, cols];
+
+            Random r = new Random();
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = r.Next(100);
+                }
+            }
+            return matrix;
+        }
+
+        private static void OfferToPrint(int rowCount, int colCount, double[,] matrix)
+        {
+            //Console.Error.Write("Computation complete. Print results (y/n)? ");
+            //char c = Console.ReadKey(true).KeyChar;
+            //Console.Error.WriteLine(c);
+            //if (Char.ToUpperInvariant(c) == 'Y')
+            //{
+            //    if (! Console.IsOutputRedirected) Console.WindowWidth = 180;
+            //    Console.WriteLine();
+            //    for (int x = 0; x < rowCount; x++)
+            //    {
+            //        Console.WriteLine("ROW {0}: ", x);
+            //        for (int y = 0; y < colCount; y++)
+            //        {
+            //            Console.Write("{0:#.##} ", matrix[x, y]);
+            //        }
+            //        Console.WriteLine();
+            //    }
+            //}
+        }
+        #endregion
     }
-    #endregion
 }
